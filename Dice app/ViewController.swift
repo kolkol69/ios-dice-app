7ccbe8b73bac72936488a0848967b2676e629634
kolkol <kolodiy1103@gmail.com>
2019-09-05 20:30:13 +0200

    Initial Commit

---
+++ Dice app/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Dice app
+//
+//  Created by Maksym Kolodiy on 05/09/2019.
+//  Copyright © 2019 Maksym Kolodiy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
